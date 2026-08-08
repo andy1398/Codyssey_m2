4ba487be24e1a4cd9cf9124027d66c26a72854cb
andy1398 <byeong3018@gmail.com>
2026-08-08 17:50:39 +0900

    문제 추가 기능

--- mission2/QuizGame.py
+++ mission2/QuizGame.py
@@ -48,3 +48,37 @@ class QuizGame:
             print("축하합니다! 최고 점수를 갱신했습니다.")
             self.best_score = score
             self.save_current_state()
+            
+    def add_quiz(self) -> None:
+        print("\n새로운 퀴즈 추가")
+        question = InputValidator.read_non_empty_string("문제 내용 입력: ")
+
+        raw_choices = InputValidator.read_non_empty_string(
+            "선택지 입력 (쉼표 ','로 구분 ex: 사과,바나나,포도,수박): "
+        )
+        choices = [c.strip() for c in raw_choices.split(",") if c.strip()]
+        """raw_choices.split(","):
+쉼표(,)를 기준으로 문자열을 잘라 리스트로 만듭니다.
+for c in ...:
+잘라낸 조각들(c)을 하나씩 순서대로 꺼냅니다
+[ 최종적으로 넣을 값 for 변수 in 반복할대상 if 조건식 ]
+        """
+        
+        while len(choices) < 4:
+            print("선택지는 최소 4개 이상이어야 합니다.")
+            raw_choices = InputValidator.read_non_empty_string("선택지 재입력 (쉼표 구분): ")
+            choices = [c.strip() for c in raw_choices.split(",") if c.strip()]
+
+        print("\n등록된 선택지:")
+        for idx, c in enumerate(choices, 1):
+            print(f"  {idx}. {c}")
+
+        answer = InputValidator.read_int_in_range(
+            f"정답 번호 선택 (1~{len(choices)}): ", 1, len(choices)
+        )
+        
+        new_quiz = Quiz(question, choices, answer)
+        self.quizzes.append(new_quiz)
+        self.save_current_state()
+        print("성공적으로 저장되었습니다!")
+ 
